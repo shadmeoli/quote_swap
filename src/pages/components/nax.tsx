c164c659c7dc3eda48c1211fe3cad81beeb778a9
shadmeoli <shadrack@shopzetu.com>
2023-12-12 08:16:25 +0300

    feat: app setup base screens

---
+++ src/pages/components/nax.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+
+// export default function Nav() {
+//     const navlinks = [
+//         { name: "Home", link: "" }
+//     ]
+//     return (
+//         <div>Nav</div>
+//     )
+
+// }
