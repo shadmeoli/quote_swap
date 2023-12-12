328da448c84b2be50497727edb0f583476df3123
shadmeoli <shadrack@shopzetu.com>
2023-12-12 18:47:08 +0300

    feat: lib folder containing some logic and simple API calls

---
+++ src/lib/quotesQuery.ts
@@ -0,0 +1,12 @@
+/**
+ * This is where the query to the API where the quotes will be placed
+ * 
+ * For now I have plugged it in directly into the componet but will be switching 
+ * and moving it here
+ */
+import axios from "axios";
+
+const API_KEY = import.meta.env.API_KEY;
+
+async function getQuotes() {
+}
